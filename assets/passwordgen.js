7ae0c181dd826a23ff0abde2d580297b6c4a821b
harlofm <nikkifm3547@gmail.com>
2021-01-24 00:21:07 -0700

    3rd attempt

--- assets/passwordgen.js
+++ assets/passwordgen.js
@@ -2,16 +2,18 @@
 
 
 
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("generate");
 
 //Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+    var password = generatePassword("generate");
+    var passwordText = document.querySelector("aasdfjkl;asdfjkl;");
 
     passwordText.value = password;
 
 }
+console.log("passwordText");
 
 // Add event listener to generate button 
 generateBtn.addEventListener("click", writePassword);
+
